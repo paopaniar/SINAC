98b2489e7a974d5e7ec8488d04326d6fb12dfda7
Paola Paniagua Arroyo <98674071+paopaniar@users.noreply.github.com>
2023-08-30 22:52:17 -0600

    Add files via upload

---
+++ Enums/Horario.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SINAC.Enums
+{
+   public enum Horario
+    {
+       GrupoA,GrupoB, GrupoC, GrupoD, GrupoE, GrupoF, GrupoG, GrupoH
+    }
+}
---
+++ Enums/Paises.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SINAC.Enums
+{
+    enum Paises
+    {
+        CostaRica, Panama, Nicaragua, Salvador, Colombia, EstadosUnidos, Alemania, Otro
+    }
+}
